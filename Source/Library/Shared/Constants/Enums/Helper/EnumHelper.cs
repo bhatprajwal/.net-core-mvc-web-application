c7b39acf5aa588024dbbeda2d2a45fd55e18c2cd
Prajwal Bhat <prajwal.neelkod@gmail.com>
2024-12-22 22:20:13 +0530

    Add Enum get select list

---
+++ Source/Library/Shared/Constants/Enums/Helper/EnumHelper.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel;
+
+namespace Constants.Enums;
+
+public static class EnumHelper
+{
+    private static string GetEnumDescription(Enum value)
+    {
+        var field = value.GetType().GetField(value.ToString());
+        var attribute = (DescriptionAttribute)Attribute.GetCustomAttribute(field, typeof(DescriptionAttribute));
+        return attribute == null ? value.ToString() : attribute.Description;
+    }
+
+    /// <summary>
+    /// Get the list of Enum items as SelectListItem
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <returns></returns>
+    public static IEnumerable<SelectListItem> GetEnumSelectList<T>() where T : Enum
+    {
+        return Enum.GetValues(typeof(T)).Cast<T>().Select(e => new SelectListItem
+        {
+            Value = ((int)(object)e).ToString(),
+            Text = GetEnumDescription(e)
+        });
+    }
+}
